c6c33f615cceb81db5099582e952ba4491021d39
Matanya Loewenthal <matanya@loewenthal.net>
2022-12-11 17:48:52 -0700

    even more

--- kubekumber.go
+++ kubekumber.go
@@ -16,13 +16,14 @@ func main() {
 	var command string
 	var regex string
 	var verbose bool
+	var dry_run bool
 
 	app := cli.NewApp()
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:        "regex,r",
-			Value:       "current",
+			Value:       getCurrentConfig(),
 			Usage:       "regex for cluster selection",
 			Destination: &regex,
 		},
@@ -37,27 +38,39 @@ func main() {
 			Usage:       "print cluster name and command before every output",
 			Destination: &verbose,
 		},
+		cli.BoolFlag{
+			Name:        "dry-run,d",
+			Usage:       "Print out clusters and command that would be run",
+			Destination: &dry_run,
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		regex_compiled := regexp.MustCompile(regex)
 
-		clusters_rune, _ := script.Exec("kubectx").MatchRegexp(regex_compiled).String()
+		var clusters []string
 
-		clusters := strings.Split(strings.TrimSpace(clusters_rune), "\n")
+		if regex == getCurrentConfig() {
+
+			clusters = []string{getCurrentConfig()}
+
+		} else {
+
+			regex_compiled := regexp.MustCompile(regex)
+
+			clusters_rune, _ := script.Exec("kubectx").MatchRegexp(regex_compiled).String()
+
+			clusters = strings.Split(strings.TrimSpace(clusters_rune), "\n")
+
+		}
 
 		for _, cluster := range clusters {
 
-			if verbose {
+			if verbose || dry_run {
 				fmt.Println("[ DEBUG ] CONTEXT: " + cluster)
 				fmt.Println("[ DEBUG ] COMMAND: kubectl " + command)
-
 			}
 
-			if regex == "current" {
-				script.Exec("kubectl " + command).Stdout()
-
-			} else {
+			if !dry_run {
 				script.Exec("kubectl " + command + " --context " + cluster).Stdout()
 			}
 
@@ -70,4 +83,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getCurrentConfig() string {
+	cluster, _ := script.Exec("kubectl config current-context").String()
+	return strings.TrimSpace(cluster)
 }
